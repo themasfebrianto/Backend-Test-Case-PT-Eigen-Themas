4da002a893bc8627715d8776dc59810b673172c0
themas <you@example.com>
2023-02-19 02:58:24 +0700

    add winston to log

--- index.js
+++ index.js
@@ -3,11 +3,14 @@ import cors from "cors";
 import dotenv from "dotenv";
 import { Sequelize } from "sequelize";
 import route from "./src/routes.js";
+import { createLogger, format, transports } from "winston";
+const { combine, timestamp, label, printf } = format;
 
 dotenv.config();
 
 const app = express();
 
+// Initialize Sequelize and test the connection
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: 'postgres',
     logging: false
@@ -22,9 +25,40 @@ sequelize
         console.error("Unable to connect to PostgreSQL:", err);
     });
 
+// Initialize logger
+const logFormat = printf(({ level, message, label, timestamp }) => {
+    return `${timestamp} [${label}] ${level}: ${message}`;
+});
+
+const logger = createLogger({
+    level: "info",
+    format: combine(
+        label({ label: "Logging" }),
+        timestamp({ format: "YYYY-MM-DD HH:mm:ss" }),
+        logFormat
+    ),
+    transports: [
+        new transports.Console(),
+        new transports.File({ filename: "error.log", level: "error" }),
+        new transports.File({ filename: "combined.log" })
+    ]
+});
+
+// Middleware for logging
+app.use((req, res, next) => {
+    logger.info(`[${req.method}] ${req.originalUrl}`);
+    next();
+});
+
 app.use(cors());
 app.use(express.json());
 app.use('/api', route);
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+    logger.error(err.stack);
+    res.status(500).send("Something went wrong.");
+});
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
